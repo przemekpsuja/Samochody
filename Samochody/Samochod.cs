47659295f5d197b06af871d9e48b380791958c69
przemekpsuja <przemyslaw.psuja@gmail.com>
2020-06-19 15:36:22 +0200

    Initial commit

---
+++ Samochody/Samochod.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Samochody
+{
+    public class Samochod : IComparable<Samochod>
+    {
+        public string Marka { set; get; }
+        public string Model { set; get; }
+        public int RokProdukcji { set; get; }
+        public decimal Cena { set; get; }
+
+        public Samochod()
+        {
+        }
+
+        public Samochod(string marka, string model, int rokProdukcji, decimal cena)
+        {
+            Marka = marka;
+            Model = model;
+            RokProdukcji = rokProdukcji;
+            Cena = cena;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Samochód {0,-10} {1,-10} cena: {2,-10} "
+                                 +
+                                 "rok produkcji {3,-10}", Marka, Model, Cena,
+                RokProdukcji);
+        }
+
+        public int CompareTo(Samochod other)
+        {
+            return Marka.CompareTo(other.Marka);
+        }
+        
+        public class PoModelu : IComparer<Samochod>
+        {
+            public int Compare(Samochod x,Samochod y)
+            {
+                return string.Compare(x.Model, y.Model, true);
+            }
+        }
+
+        public class PoCenie : IComparer<Samochod>
+        {
+            public int Compare(Samochod x, Samochod y)
+            {
+                return x.Cena.CompareTo(y.Cena);
+            }
+        }
+
+        public class PoRokuProdukcji : IComparer<Samochod>
+        {
+            public int Compare(Samochod x, Samochod y)
+            {
+                return x.RokProdukcji.CompareTo(y.RokProdukcji);
+            }
+        }
+        
+        
+
+    }
+}
---
+++ TestSamochodu/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Samochody;
+
+namespace TestSamochodu
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Samochod> komis = new List<Samochod>(100)
+            {
+                new Samochod()
+                {
+                    Marka = "Audi", Model = "A3", RokProdukcji = 2007,
+                    Cena = 100000
+                },
+                new Samochod()
+                {
+                    Marka = "Opel", Model = "Vectra", RokProdukcji = 2000,
+                    Cena = 10000
+                },
+                new Samochod()
+                {
+                    Marka = "Syrena", Model = "105L", RokProdukcji = 1978,
+                    Cena = 100
+                },
+                new Samochod()
+                {
+                    Marka = "Fiat", Model = "125", RokProdukcji = 1980,
+                    Cena = 105
+                }
+            };
+            char c;
+            do
+            {
+                c = Menu();
+                switch (c)
+                {
+                    case 'a':
+                    case 'A':
+                        komis.Add(DodajSamochod());
+                        break;
+                    case 'b':
+                    case 'B':
+                        komis.Sort();
+                        WypiszSamochody(komis);
+                        break;
+                    case 'c':
+                    case 'C':
+                        komis.Sort(new Samochod.PoRokuProdukcji());
+                        WypiszSamochody(komis);
+                        break;
+                    case 'd':
+                    case 'D':
+                        komis.Sort(new Samochod.PoModelu());
+                        WypiszSamochody(komis);
+                        break;
+                    case 'e':
+                    case 'E':
+                        komis.Sort(new Samochod.PoCenie());
+                        WypiszSamochody(komis);
+                        break;
+                }
+            } 
+            while (!(c == 'k' || c == 'K'));
+        }
+        
+        public static void WypiszSamochody(List<Samochod> komis)
+        {
+            Console.Clear();
+            for (int i = 1; i <= komis.Count; i++)
+            {
+                Console.WriteLine("{0,3}. {1}", i, komis[i - 1]);
+                if (i % Console.WindowHeight == 0)
+                    Console.ReadKey(false);
+            }
+
+            Console.ReadKey();
+        }
+
+        public static char Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("A - Dodaj samochód");
+            Console.WriteLine("B - Wypisz wedug nazwy marki");
+            Console.WriteLine("C - Wypisz według roku produkcji");
+            Console.WriteLine("D - Wypisz według nazwy modelu");
+            Console.WriteLine("E - Wypisz według ceny");
+            Console.WriteLine("K - Koniec");
+            return Console.ReadKey().KeyChar;
+        }
+
+        private static Samochod DodajSamochod()
+        {
+            Console.WriteLine("Podaj markę samochodu: ");
+            string marka = Console.ReadLine();
+            Console.WriteLine("Podaj model samochodu: ");
+            string model = Console.ReadLine();
+            decimal cena;
+            do
+            {
+                Console.WriteLine("Podaj cenę samochodu: ");
+            } while (!decimal.TryParse(Console.ReadLine(), out cena));
+
+            int rokProdukcji;
+            do
+            {
+                Console.WriteLine("Podaj rok produkcji samochodu: ");
+            } while (!int.TryParse(Console.ReadLine(), out rokProdukcji));
+
+            return new Samochod()
+            {
+                Cena = cena,
+                RokProdukcji = rokProdukcji,
+                Model = model,
+                Marka = marka
+            };
+        }
+    }
+}
